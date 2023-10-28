c73ac0cd9b7715313dfa535e1dcd9d144722a50d
Stephen Karney <112995644+Ginger-BeardMan@users.noreply.github.com>
2023-10-28 16:41:21 -0400

    Create pomodoro_timer.py

---
+++ pomodoro_timer.py
@@ -0,0 +1,44 @@
+from tkinter import *
+
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+
+# ---------------------------- TIMER RESET ------------------------------- #
+
+# ---------------------------- TIMER MECHANISM ------------------------------- #
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0) # draw on the window using canvas
+tomato_img = PhotoImage(file='tomato.png') # use photoimage to read a photo file to use in the program
+canvas.create_image(100, 112, image=tomato_img) # set image to be used and location on screen
+canvas.create_text(100, 140, text='00:00', fill='white', font=(FONT_NAME, 35, "bold"))
+canvas.grid(column=1, row=1)
+
+start_button = Button(text='Start', width=3)
+start_button.grid(column=0, row=2)
+
+reset_button = Button(text='Reset', width=3)
+reset_button.grid(column=2, row=2)
+
+check_marks = Label(text='check', bg=YELLOW)
+check_marks.grid(column=1, row=2)
+
+timer_title = Label(text='Timer', fg=GREEN, font=(FONT_NAME, 45, "bold"), bg=YELLOW)
+timer_title.grid(column=1, row=0)
+
+window.mainloop()
