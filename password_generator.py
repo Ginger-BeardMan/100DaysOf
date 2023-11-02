84b3c8ba81fbd26244c9d4e3a9afab1ee524becf
Stephen Karney <112995644+Ginger-BeardMan@users.noreply.github.com>
2023-11-02 08:09:23 -0400

    Update password_generator.py
    modified save password function to account for empty fields, adding password generator function

--- password_generator.py
+++ password_generator.py
@@ -1,8 +1,35 @@
-import tkinter
 from tkinter import *
+from tkinter import messagebox
+import random
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+# Password Generator Project
 
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+           'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+           'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+nr_letters = random.randint(8, 10)
+nr_symbols = random.randint(2, 4)
+nr_numbers = random.randint(2, 4)
+
+new_letters = [random.choice(letters) for let in range(nr_letters)]
+
+new_symbols = [random.choice(symbols) for sym in range(nr_symbols)]
+
+new_numbers = [random.choice(numbers) for num in range(nr_numbers)]
+
+password_list = new_letters + new_symbols + new_numbers
+
+random.shuffle(password_list)
+
+password = ""
+for char in password_list:
+    password += char
+
+print(f"Your password is: {password}")
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 
@@ -12,12 +39,19 @@ def save_information():
     username_email = email_username_entry.get()
     password = new_pass_result.get()
 
-    with open("data.txt", 'a') as new_passwords:
-        new_passwords.write(f"{website_address} | {username_email} | {password}\n")
+    if len(website_address) == 0 or len(password) == 0:
+        messagebox.showinfo(title='Warning', message="Please don't leave any fields empty!")
 
-    website_entry.delete(0, END)
-    email_username_entry.delete(0, END)
-    new_pass_result.delete(0, END)
+    else:
+        is_ok = messagebox.askokcancel(title=website_address,
+                                       message=f'These are the details entered:\nEmail: {username_email}'
+                                               f'\n Password: {password} \n Is this okay to save?')
+
+        if is_ok:
+            with open("data.txt", 'a') as new_passwords:
+                new_passwords.write(f"{website_address} | {username_email} | {password}\n")
+                website_entry.delete(0, END)
+                new_pass_result.delete(0, END)
 
 # ---------------------------- UI SETUP ------------------------------- #
 
