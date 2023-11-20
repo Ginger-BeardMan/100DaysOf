8a4c840a1a9c6ebc8430068658ec4b5932d9defb
Stephen Karney <112995644+Ginger-BeardMan@users.noreply.github.com>
2023-11-20 06:53:37 -0500

    Update ISS_Locator.py
    finalized checks on location and sunlight
    
    both conditions are now checked every 60 seconds, if both are true then an 'email' is sent (email and password will need to be changed to real email and password if used)

--- ISS_Locator.py
+++ ISS_Locator.py
@@ -1,13 +1,24 @@
 import requests
 from datetime import datetime
+import smtplib
+import time
 
+# Constants
 MY_LAT = 42.361145
 MY_LONG = -71.057083
+
 up_lat = MY_LAT + 5
 low_lat = MY_LAT - 5
 up_long = MY_LONG + 5
 low_long = MY_LONG - 5
 
+# Sender Contact Information
+my_email = 'chungus3535@gmail.com'
+password = 'abcd1357)('
+
+my_second_email = 'bungus3535@gmail.com'
+
+# ISS location
 response = requests.get(url='http://api.open-notify.org/iss-now.json')
 response.raise_for_status()
 
@@ -18,17 +29,7 @@ iss_latitude = float(data['iss_position']['latitude'])
 
 iss_position = (iss_latitude, iss_longitude)
 
-# create function that returns true if:
-# your position is within +5 or -5 degrees of the ISS position
-
-
-def check_location():
-    if low_lat < iss_latitude < up_lat and low_long < iss_longitude < up_long:
-        return True
-    else:
-        return False
-
-
+# Sunrise/Sunset based on location
 parameters = {
     'lat': MY_LAT,
     'long': MY_LONG,
@@ -45,17 +46,31 @@ time_now = datetime.now()
 current_hour = time_now.hour
 
 
+def check_location():
+    if low_lat < iss_latitude < up_lat and low_long < iss_longitude < up_long:
+        return True
+    else:
+        return False
+
+
 def check_light():
     if sunset < current_hour < sunrise:
         return True
     else:
         return False
 
-# if the ISS is close to my current position
-# and it is currently dark
-# then send me an email to tell me to look up
+# if the ISS is close to my current position, and it is currently dark then send a message to tell me to look up
 # BONUS: run the code every 60 seconds
 
 
-check_location()
-check_light()
+def send_message():
+    if check_location() and check_light():
+        with smtplib.SMTP('smtp.gmail.com') as connection:
+            connection.starttls()
+            connection.login(user=my_email, password=password)
+            connection.sendmail(from_addr=my_email, to_addrs=my_second_email, msg="Look Up!")
+    time.sleep(60)
+    send_message()
+
+
+send_message()
