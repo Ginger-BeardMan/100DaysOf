cb53d5734def111b64c3eb15cee58802b139a682
Stephen Karney <112995644+Ginger-BeardMan@users.noreply.github.com>
2023-12-05 21:23:56 -0500

    Update nutrition_tracker.py
    updated code with os module and set important variables to environmental variables.

--- nutrition_tracker.py
+++ nutrition_tracker.py
@@ -1,5 +1,6 @@
 import requests
 from datetime import datetime
+import os
 
 DATE = datetime.now()
 TIME = datetime.time(DATE)
@@ -8,34 +9,25 @@ current_date = str(DATE.strftime("%m/%d/%Y"))
 current_time = str(TIME.strftime('%-H:%-M:%S'))
 
 # ------------------------------------------------- NutritionIX API -------------------------------------------------
-APP_ID = # YOUR APP_ID
-
-API_KEY = # YOUR API_KEY
-
-GENDER = # YOUR GENDER
-WEIGHT_KG = # YOUR WEIGHT IN KG
-HEIGHT_CM = # YOUR HEIGHT IN CM
-AGE = # YOUR AGE IN YEARS
-
-exercise_stats_endpoint = 'https://trackapi.nutritionix.com/v2/natural/exercise'
 
 exercise_input = input('Tell me which exercise you did: ')
 
 exercise_headers = {
-    'x-app-id': APP_ID,
-    'x-app-key': API_KEY,
+    'x-app-id': os.environ['APP_ID'],
+    'x-app-key': os.environ['API_KEY'],
     'Content-Type': 'application/json'
 }
 
 exercise_config = {
     "query": exercise_input,
-    "gender": GENDER,
-    "weight_kg": WEIGHT_KG,
-    "height_cm": HEIGHT_CM,
-    "age": AGE,
+    "gender": os.environ['GENDER'],
+    "weight_kg": os.environ['WEIGHT_KG'],
+    "height_cm": os.environ['HEIGHT_CM'],
+    "age": os.environ['AGE'],
 }
+exercise_stats_endpoint = 'https://trackapi.nutritionix.com/v2/natural/exercise'
 
-nutrition_response = requests.post(exercise_stats_endpoint, headers=exercise_headers, json=exercise_config)
+nutrition_response = requests.post(url=exercise_stats_endpoint, headers=exercise_headers, json=exercise_config)
 nutrition_response.raise_for_status()
 nutrition_result = nutrition_response.json()['exercises'][0]
 
@@ -44,10 +36,6 @@ duration = str(nutrition_result['duration_min'])
 calories = str(nutrition_result['nf_calories'])
 
 # --------------------------------------------------- Sheety API ---------------------------------------------------
-
-BEARER_TOKEN = # YOUR BEARER TOKEN
-
-sheety_workouts_endpoint = # YOUR SHEETY API ENDPOINT
 
 sheety_config = {
     'workout': {
@@ -60,8 +48,9 @@ sheety_config = {
 }
 
 header = {
-    "Authorization": f'Bearer {BEARER_TOKEN}'
+    "Authorization": f'Bearer {os.environ["BEARER_TOKEN"]}'
 }
 
-sheety_response = requests.post(sheety_workouts_endpoint, json=sheety_config, headers=header)
+sheety_response = requests.post(url=os.environ['sheety_workouts_endpoint'], json=sheety_config, headers=header)
 sheety_response.raise_for_status()
+
