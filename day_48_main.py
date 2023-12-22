36a18cedabf71599d33eb0cfb6322a3185c51568
Stephen Karney <112995644+Ginger-BeardMan@users.noreply.github.com>
2023-12-22 09:52:55 -0500

    Update day_48_main.py
    attempting to scrape a webpage and make a nested dictionary for upcoming events using selenium webdriver

--- day_48_main.py
+++ day_48_main.py
@@ -2,10 +2,35 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 # keep browser open after program finishes
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_experimental_option('detach', True)
+firefox_options = webdriver.FirefoxOptions()
+# chrome_options.add_experimental_option('detach', True)
 
-driver = webdriver.Chrome(options=chrome_options)
+driver = webdriver.Firefox(options=firefox_options)
+
+driver.get('https://python.org')
+
+nums = [0, 1, 2, 3, 4]
+
+keys = ['time', 'name', 'time', 'name', 'time', 'name', 'time', 'name', 'time', 'name']
+
+full_list = driver.find_element(By.XPATH, value='//*[@id="content"]/div/section/div[3]/div[2]/div/ul')
+
+item_list = full_list.text.split('\n')
+
+upcoming_events = {num: {k: v for (k, v) in (keys, item_list)} for num in nums}
+
+print(upcoming_events)
+
+# rand_dict = {key: value for (key, value) in zip(keys, item_list)}
+#
+# print(rand_dict)
+
+# driver.close()
+# driver.quite()
+
+
+# # PRACTICE CODE # #
+
 # driver.get('https://www.amazon.com/Ultra-Sharp-Diamond-Sharpening-Stone/dp/B00SK3LDBE/ref=as_li_ss_tl?ie=UTF8&sr'
 #            '=8-13&keywords=diamond+stone&linkId=90f4e83f38af9a75007c65a020859a74&language=en_US')
 #
@@ -13,15 +38,11 @@ driver = webdriver.Chrome(options=chrome_options)
 # price_cents = driver.find_element(By.CLASS_NAME, value='a-price-fraction')
 # print(f'The price is {price_dollar.text}.{price_cents.text}')
 
-driver.get('https://python.org')
 # search_bar = driver.find_element(By.NAME, value='q')
 # print(search_bar.get_attribute('placeholder'))
 #
 # button = driver.find_element(By.ID, value='submit')
 # print(button.size)
 
-bug_link = driver.find_element(By.XPATH, value='//*[@id="site-map]/div[2]/div/ul/li[3]/a')
-print(bug_link.text)
-
-# driver.close()
-# driver.quite()
+# bug_link = driver.find_element(By.XPATH, value='//*[@id="site-map]/div[2]/div/ul/li[3]/a')
+# print(bug_link.text)
