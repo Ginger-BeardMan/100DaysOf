ae6167eae17edc171e65a5ef49577a3eb96205de
Stephen Karney <112995644+Ginger-BeardMan@users.noreply.github.com>
2023-12-30 18:48:56 -0500

    Update day_48_cookie_clicker.py
    added double click ability

--- day_48_cookie_clicker.py
+++ day_48_cookie_clicker.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
 from time import sleep
 
 URL = 'http://orteil.dashnet.org/experiments/cookie/'
@@ -7,6 +8,8 @@ URL = 'http://orteil.dashnet.org/experiments/cookie/'
 firefox_options = webdriver.FirefoxOptions()
 driver = webdriver.Firefox(options=firefox_options)
 driver.get(URL)
+
+actionChains = ActionChains(driver)
 
 cookie_time = True
 
@@ -22,46 +25,45 @@ cookie_money = int(cookie_clicks.text)
 time_machine_element = driver.find_element(By.XPATH, value='//*[@id="buyTime machine"]/b')
 time_machine = int(time_machine_element.text.split()[3].replace(',', ''))
 
-portal_element = driver.find_element(By.ID, value='//*[@id="buyPortal"]/b')
+portal_element = driver.find_element(By.XPATH, value='//*[@id="buyPortal"]/b')
 portal = int(portal_element.text.split()[2].replace(',', ''))
 
-alchemy_lab_element = driver.find_element(By.ID, value='//*[@id="buyAlchemy lab"]/b')
+alchemy_lab_element = driver.find_element(By.XPATH, value='//*[@id="buyAlchemy lab"]/b')
 alchemy_lab = int(alchemy_lab_element.text.split()[3].replace(',', ''))
 
-shipment_element = driver.find_element(By.ID, value='//*[@id="buyShipment"]/b')
+shipment_element = driver.find_element(By.XPATH, value='//*[@id="buyShipment"]/b')
 shipment = int(shipment_element.text.split()[2].replace(',', ''))
 
-mine_element = driver.find_element(By.ID, value='//*[@id="buyMine"]/b')
+mine_element = driver.find_element(By.XPATH, value='//*[@id="buyMine"]/b')
 mine = int(mine_element.text.split()[2].replace(',', ''))
 
-factory_element = driver.find_element(By.ID, value='//*[@id="buyFactory"]/b')
+factory_element = driver.find_element(By.XPATH, value='//*[@id="buyFactory"]/b')
 factory = int(factory_element.text.split()[2].replace(',', ''))
 
-grandma_element = driver.find_element(By.ID, value='//*[@id="buyGrandma"]/b')
+grandma_element = driver.find_element(By.XPATH, value='//*[@id="buyGrandma"]/b')
 grandma = int(grandma_element.text.split()[2].replace(',', ''))
 
-cursor_element = driver.find_element(By.ID, value='//*[@id="buyCursor"]/b')
+cursor_element = driver.find_element(By.XPATH, value='//*[@id="buyCursor"]/b')
 cursor = int(cursor_element.text.split()[2].replace(',', ''))
 
 
 def check_costs():
-    sleep(300)
     if cookie_money >= time_machine:
-        time_machine_element.click()
+        actionChains.double_click(time_machine_element)
     elif cookie_money >= portal:
-        portal_element.click()
+        actionChains.double_click(portal_element)
     elif cookie_money >= alchemy_lab:
-        alchemy_lab_element.click()
+        actionChains.double_click(alchemy_lab_element)
     elif cookie_money >= shipment:
-        shipment_element.click()
+        actionChains.double_click(shipment_element)
     elif cookie_money >= mine:
-        mine_element.click()
+        actionChains.double_click(mine_element)
     elif cookie_money >= factory:
-        factory_element.click()
+        actionChains.double_click(factory_element)
     elif cookie_money >= grandma:
-        grandma_element.click()
+        actionChains.double_click(grandma_element)
     elif cookie_money >= cursor:
-        cursor_element.click()
+        actionChains.double_click(cursor_element)
 
 
 while cookie_time:
