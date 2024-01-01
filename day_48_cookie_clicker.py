0d27d4b927e265a3a42950c968758255f1d4b658
Stephen Karney <112995644+Ginger-BeardMan@users.noreply.github.com>
2024-01-01 06:39:34 -0500

    Update day_48_cookie_clicker.py
    the program now automatically runs (clicking and buying every 5 seconds). The code needs to be cleaned up before finalized.

--- day_48_cookie_clicker.py
+++ day_48_cookie_clicker.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
-from time import sleep
+import time
 
 URL = 'http://orteil.dashnet.org/experiments/cookie/'
 
@@ -11,69 +11,71 @@ driver.get(URL)
 
 actionChains = ActionChains(driver)
 
-cookie_time = True
 
-# ------------------------------------------------------- Items -------------------------------------------------------
-click_cookie = driver.find_element(By.ID, value='cookie')
+def cookie_clicker():
+    cookie_time = True
+    start_time = time.time()
+    while cookie_time:
+        click_cookie = driver.find_element(By.ID, value='cookie')
 
-cookie_clicks = driver.find_element(By.XPATH, value='//*[@id="money"]')
-cookie_money = int(cookie_clicks.text)
+        cookie_clicks = driver.find_element(By.XPATH, value='//*[@id="money"]')
+        cookie_money = int(cookie_clicks.text)
 
-# elder_pledge_element = driver.find_element(By.XPATH, value='//*[@id="buyElder Pledge"]/b')
-# elder_pledge = int(elder_pledge_element.text.split()[1])
+        time_machine_element = driver.find_element(By.XPATH, value='//*[@id="buyTime machine"]/b')
+        time_machine_money = int(time_machine_element.text.split()[3].replace(',', ''))
+        time_machine_button = driver.find_element(By.ID, value="buyTime machine")
 
-time_machine_element = driver.find_element(By.XPATH, value='//*[@id="buyTime machine"]/b')
-time_machine_money = int(time_machine_element.text.split()[3].replace(',', ''))
-time_machine_button = driver.find_element(By.ID, value="buyTime machine")
+        portal_element = driver.find_element(By.XPATH, value='//*[@id="buyPortal"]/b')
+        portal_money = int(portal_element.text.split()[2].replace(',', ''))
+        portal_button = driver.find_element(By.ID, value='buyPortal')
 
-portal_element = driver.find_element(By.XPATH, value='//*[@id="buyPortal"]/b')
-portal_money = int(portal_element.text.split()[2].replace(',', ''))
-portal_button = driver.find_element(By.ID, value='buyPortal')
+        alchemy_lab_element = driver.find_element(By.XPATH, value='//*[@id="buyAlchemy lab"]/b')
+        alchemy_lab_money = int(alchemy_lab_element.text.split()[3].replace(',', ''))
+        alchemy_lab_button = driver.find_element(By.ID, value='buyAlchemy lab')
 
-alchemy_lab_element = driver.find_element(By.XPATH, value='//*[@id="buyAlchemy lab"]/b')
-alchemy_lab_money = int(alchemy_lab_element.text.split()[3].replace(',', ''))
-alchemy_lab_button = driver.find_element(By.ID, value='buyAlchemy lab')
+        shipment_element = driver.find_element(By.XPATH, value='//*[@id="buyShipment"]/b')
+        shipment_money = int(shipment_element.text.split()[2].replace(',', ''))
+        shipment_button = driver.find_element(By.ID, value='buyShipment')
 
-shipment_element = driver.find_element(By.XPATH, value='//*[@id="buyShipment"]/b')
-shipment_money = int(shipment_element.text.split()[2].replace(',', ''))
-shipment_button = driver.find_element(By.ID, value='buyShipment')
+        mine_element = driver.find_element(By.XPATH, value='//*[@id="buyMine"]/b')
+        mine_money = int(mine_element.text.split()[2].replace(',', ''))
+        mine_button = driver.find_element(By.ID, value='buyMine')
 
-mine_element = driver.find_element(By.XPATH, value='//*[@id="buyMine"]/b')
-mine_money = int(mine_element.text.split()[2].replace(',', ''))
-mine_button = driver.find_element(By.ID, value='buyMine')
+        factory_element = driver.find_element(By.XPATH, value='//*[@id="buyFactory"]/b')
+        factory_money = int(factory_element.text.split()[2].replace(',', ''))
+        factory_button = driver.find_element(By.ID, value='buyFactory')
 
-factory_element = driver.find_element(By.XPATH, value='//*[@id="buyFactory"]/b')
-factory_money = int(factory_element.text.split()[2].replace(',', ''))
-factory_button = driver.find_element(By.ID, value='buyFactory')
+        grandma_element = driver.find_element(By.XPATH, value='//*[@id="buyGrandma"]/b')
+        grandma_money = int(grandma_element.text.split()[2].replace(',', ''))
+        grandma_button = driver.find_element(By.ID, value='buyGrandma')
 
-grandma_element = driver.find_element(By.XPATH, value='//*[@id="buyGrandma"]/b')
-grandma_money = int(grandma_element.text.split()[2].replace(',', ''))
-grandma_button = driver.find_element(By.ID, value='buyGrandma')
+        cursor_element = driver.find_element(By.XPATH, value='//*[@id="buyCursor"]/b')
+        cursor_money = int(cursor_element.text.split()[2].replace(',', ''))
+        cursor_button = driver.find_element(By.ID, value='buyCursor')
 
-cursor_element = driver.find_element(By.XPATH, value='//*[@id="buyCursor"]/b')
-cursor_money = int(cursor_element.text.split()[2].replace(',', ''))
-cursor_button = driver.find_element(By.ID, value='buyCursor')
+        click_cookie.click()
+
+        stop_time = time.time()
+
+        if stop_time - start_time >= 600:
+            cookie_time = False
+        elif stop_time - start_time >= 5:
+            if int(cookie_clicks.text) >= time_machine_money:
+                time_machine_button.click()
+            elif int(cookie_clicks.text) >= portal_money:
+                portal_button.click()
+            elif int(cookie_clicks.text) >= alchemy_lab_money:
+                alchemy_lab_button.click()
+            elif int(cookie_clicks.text) >= shipment_money:
+                shipment_button.click()
+            elif int(cookie_clicks.text) >= mine_money:
+                mine_button.click()
+            elif int(cookie_clicks.text) >= factory_money:
+                factory_button.click()
+            elif int(cookie_clicks.text) >= grandma_money:
+                grandma_button.click()
+            elif int(cookie_clicks.text) >= cursor_money:
+                cursor_button.click()
 
 
-# def check_costs():
-
-while cookie_time:
-    click_cookie.click()
-    if int(cookie_clicks.text) >= time_machine_money:
-        time_machine_button.click()
-    elif int(cookie_clicks.text) >= portal_money:
-        portal_button.click()
-    elif int(cookie_clicks.text) >= alchemy_lab_money:
-        alchemy_lab_button.click()
-    elif int(cookie_clicks.text) >= shipment_money:
-        shipment_button.click()
-    elif int(cookie_clicks.text) >= mine_money:
-        mine_button.click()
-    elif int(cookie_clicks.text) >= factory_money:
-        factory_button.click()
-    elif int(cookie_clicks.text) >= grandma_money:
-        grandma_button.click()
-    elif int(cookie_clicks.text) >= cursor_money:
-        cursor_button.click()
-    elif int(cookie_clicks.text) >= 100000:
-        cookie_time = False
+cookie_clicker()
