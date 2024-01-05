6c50e764e42a11fb1993dc325405aa497e19de75
Stephen Karney <112995644+Ginger-BeardMan@users.noreply.github.com>
2024-01-05 15:17:09 -0500

    Update day_53_main.py
    cleaned up addresses in selenium webdriver, attempted to gather links and addresses using beautiful soup

--- day_53_main.py
+++ day_53_main.py
@@ -3,6 +3,8 @@ from selenium.webdriver.common.by import By
 import requests
 from bs4 import BeautifulSoup
 
+FORMS_LINK = YOUR_FORMS_LINK
+
 URL = 'https://appbrewery.github.io/Zillow-Clone/'
 
 # ----------------------------------------------------Beautiful Soup----------------------------------------------------
@@ -10,8 +12,17 @@ response = requests.get(URL)
 zillow_clone_webpage = response.text
 soup = BeautifulSoup(zillow_clone_webpage, 'html.parser')
 
+links = soup.find_all('a')
 
-# --------------------------------------------------Selenium Webdriver--------------------------------------------------
+for link in links:
+    print(link.get('href'))
+
+addresses = soup.find_all('address')
+
+for address in addresses:
+    print(address.get_text())
+
+--------------------------------------------------Selenium Webdriver--------------------------------------------------
 firefox_options = webdriver.FirefoxOptions()
 driver = webdriver.Firefox(options=firefox_options)
 driver.get(URL)
@@ -23,5 +34,7 @@ for n in property_list_element:
     property_price = n.find_element(By.TAG_NAME, value='span')
     property_link = n.find_element(By.TAG_NAME, value='a')
     print(property_address.text)
-    print(property_price.text)
+    print(property_price.text.replace('$', '').replace('+', '').replace('/mo', '').replace('1 bd', '')
+          .replace('1bd', ''))
     print(property_link.get_attribute('href'))
+
