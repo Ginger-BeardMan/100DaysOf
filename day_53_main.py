8be363a55d5d9c018f791f592c87e4375c28075c
Stephen Karney <112995644+Ginger-BeardMan@users.noreply.github.com>
2024-01-07 06:20:59 -0500

    Update day_53_main.py
    added text input elements and am attempting to cycle through lists to input information to the google form.

--- day_53_main.py
+++ day_53_main.py
@@ -3,12 +3,12 @@ from selenium.webdriver.common.by import By
 import requests
 from bs4 import BeautifulSoup
 
-FORMS_LINK = YOUR_FORMS_LINK
+FORMS_URL = YOUR_FORMS_LINK
 
-URL = 'https://appbrewery.github.io/Zillow-Clone/'
+ZILLOW_URL = 'https://appbrewery.github.io/Zillow-Clone/'
 
 # ----------------------------------------------------Beautiful Soup----------------------------------------------------
-response = requests.get(URL)
+response = requests.get(ZILLOW_URL)
 zillow_clone_webpage = response.text
 soup = BeautifulSoup(zillow_clone_webpage, 'html.parser')
 
@@ -34,16 +34,36 @@ for price in page_prices:
 --------------------------------------------------Selenium Webdriver--------------------------------------------------
 firefox_options = webdriver.FirefoxOptions()
 driver = webdriver.Firefox(options=firefox_options)
-driver.get(URL)
+driver.get(FORMS_URL)
 
-property_list_element = driver.find_elements(By.CLASS_NAME, value='ListItem-c11n-8-84-3-StyledListCardWrapper')
+q_1 = driver.find_element(By.XPATH, value='/html/body/div/div[2]/form/div[2]/div/div[2]/div[1]/div/div/div[2]/div/'
+                                          'div[1]/div/div[1]/input')
 
-for n in property_list_element:
-    property_address = n.find_element(By.TAG_NAME, value='address')
-    property_price = n.find_element(By.TAG_NAME, value='span')
-    property_link = n.find_element(By.TAG_NAME, value='a')
-    print(property_address.text)
-    print(property_price.text.replace('$', '').replace('+', '').replace('/mo', '').replace('1 bd', '')
-          .replace('1bd', ''))
-    print(property_link.get_attribute('href'))
+q_2 = driver.find_element(By.XPATH, value='/html/body/div/div[2]/form/div[2]/div/div[2]/div[2]/div/div/div[2]/div/'
+                                          'div[1]/div/div[1]/input')
+
+q_3 = driver.find_element(By.XPATH, value='/html/body/div/div[2]/form/div[2]/div/div[2]/div[2]/div/div/div[2]/div/'
+                                          'div[1]/div/div[1]/input')
+
+submit_button = driver.find_element(By.XPATH, value='/html/body/div/div[2]/form/div[2]/div/div[3]/div[1]/div[1]/div'
+                                                    '/span/span')
+
+# for n in range(44):
+#     q_1.send_keys(addresses[n])
+#     q_2.send_keys(prices[n])
+#     q_3.send_keys(links[n])
+#     submit_button.click()
+
+
+
+# property_list_element = driver.find_elements(By.CLASS_NAME, value='ListItem-c11n-8-84-3-StyledListCardWrapper')
+
+# for n in property_list_element:
+#     property_address = n.find_element(By.TAG_NAME, value='address')
+#     property_price = n.find_element(By.TAG_NAME, value='span')
+#     property_link = n.find_element(By.TAG_NAME, value='a')
+#     print(property_address.text)
+#     print(property_price.text.replace('$', '').replace('+', '').replace('/mo', '').replace('1 bd', '')
+#           .replace('1bd', ''))
+#     print(property_link.get_attribute('href'))
 
