3e4304f1786394f54b4f761780cd3c9908eb94d0
Stephen Karney <112995644+Ginger-BeardMan@users.noreply.github.com>
2024-01-10 07:09:32 -0500

    Update day_54_flask.py

--- day_54_flask.py
+++ day_54_flask.py
@@ -5,3 +5,37 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+# exercise for decorator functions
+import time
+
+current_time = time.time()
+print(current_time)  # seconds since Jan 1st, 1970
+
+
+def speed_calc_decorator(function):
+    def calc_speed():
+        start_time = time.time()
+        function()
+        end_time = time.time()
+        print(end_time - start_time)
+
+    return calc_speed
+
+
+@speed_calc_decorator
+def fast_function():
+    for i in range(1000000):
+        i * i
+
+
+@speed_calc_decorator
+def slow_function():
+    for i in range(10000000):
+        i * i
+
+
+fast_function()
+slow_function()
+
